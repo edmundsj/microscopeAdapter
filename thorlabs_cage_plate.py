c7b962d20aa75eade7cb0fbc1e9b7a306d7d1c11
edmundsj <jordan.e@berkeley.edu>
2020-10-05 13:53:40 -0700

    added scad, stl, and python script to generate file

---
+++ thorlabs_cage_plate.py
@@ -0,0 +1,40 @@
+"""
+Creates a near-copy of the thorlabs CP33, modified for one side to mate to a 38mm microscope tube
+"""
+import openpyscad as ops
+plate_width = 45
+plate_thickness = 8.9
+hole_pitch = 30
+hole_length = plate_thickness + 1
+hole_offset = 1
+rod_diameter = 6.1
+rod_hole_diameter = rod_diameter
+screw_hole_diameter = 2.5
+wall_thickness = 3.0
+center_bore_od = 37.8
+protrusion_length = 15
+
+cage_plate = ops.Cube([plate_width, plate_width, plate_thickness])
+cage_plate = cage_plate.translate([-plate_width/2, -plate_width/2, 0])
+rod_holes = [ops.Cylinder(h=hole_length, d=rod_hole_diameter, fn=20) for x in range(4)]
+rod_holes[0] = rod_holes[0].translate([hole_pitch/2, hole_pitch/2, hole_offset])
+rod_holes[1] = rod_holes[1].translate([-hole_pitch/2, hole_pitch/2, hole_offset])
+rod_holes[2] = rod_holes[2].translate([hole_pitch/2, -hole_pitch/2, hole_offset])
+rod_holes[3] = rod_holes[3].translate([-hole_pitch/2, -hole_pitch/2, hole_offset])
+inner_holes = [ops.Cylinder(h=plate_thickness, d=screw_hole_diameter) for x in range(4)]
+inner_holes[0] = inner_holes[0].translate([hole_pitch/2, hole_pitch/2, -hole_offset])
+inner_holes[1] = inner_holes[1].translate([-hole_pitch/2, hole_pitch/2, -hole_offset])
+inner_holes[2] = inner_holes[2].translate([hole_pitch/2, -hole_pitch/2, -hole_offset])
+inner_holes[3] = inner_holes[3].translate([-hole_pitch/2, -hole_pitch/2, -hole_offset])
+
+microscope_cylinder = ops.Cylinder(d=center_bore_od, h=protrusion_length + plate_thickness)
+microscope_cylinder = microscope_cylinder.translate([0, 0, -protrusion_length])
+center_bore = ops.Cylinder(d=center_bore_od - 2 * wall_thickness, h=protrusion_length + plate_thickness + 1)
+center_bore = center_bore.translate([0, 0, -protrusion_length - 0.5])
+
+cage_plate = cage_plate + microscope_cylinder - center_bore - \
+        rod_holes[0] - rod_holes[1] - rod_holes[2] - rod_holes[3] - \
+        inner_holes[0] - inner_holes[1] - inner_holes[2] - inner_holes[3]
+
+
+cage_plate.write('thorlabs_microscope_adapter.scad')
---
+++ thorlabs_microscope_adapter.scad
@@ -0,0 +1,39 @@
+$fa=0.4;
+$fs=0.5;
+difference(){
+    union(){
+        translate(v=[-22.5, -22.5, 0]){
+            cube(size=[45, 45, 8.9]);
+        };
+        translate(v=[0, 0, -15]){
+            cylinder(h=23.9, d=37.8);
+        };
+    };
+    translate(v=[0, 0, -15.5]){
+        cylinder(h=24.9, d=31.799999999999997);
+    };
+    translate(v=[15.0, 15.0, 1]){
+        cylinder(h=9.9, d=6.1);
+    };
+    translate(v=[-15.0, 15.0, 1]){
+        cylinder(h=9.9, d=6.1);
+    };
+    translate(v=[15.0, -15.0, 1]){
+        cylinder(h=9.9, d=6.1);
+    };
+    translate(v=[-15.0, -15.0, 1]){
+        cylinder(h=9.9, d=6.1);
+    };
+    translate(v=[15.0, 15.0, -1]){
+        cylinder(h=8.9, d=2.5);
+    };
+    translate(v=[-15.0, 15.0, -1]){
+        cylinder(h=8.9, d=2.5);
+    };
+    translate(v=[15.0, -15.0, -1]){
+        cylinder(h=8.9, d=2.5);
+    };
+    translate(v=[-15.0, -15.0, -1]){
+        cylinder(h=8.9, d=2.5);
+    };
+};
